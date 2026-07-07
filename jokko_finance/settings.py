698c9e985426b8e14480d88f18cbb00ab5e1f959
Kokoriko <dplionnel@gmail.com>
2026-07-07 13:56:59 +0000

    change db settings

--- jokko_finance/settings.py
+++ jokko_finance/settings.py
@@ -66,13 +66,8 @@ WSGI_APPLICATION = 'jokko_finance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "samaxalis_db",
-        'HOST': '127.0.0.1',
-        'POST': '3306',
-        'USER': 'root',
-        'PASSWORD': ''
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
