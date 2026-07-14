c12f10ae10bf88400d3ac84c7242c8293fceb98a
Kokoriko <dplionnel@gmail.com>
2026-07-14 22:46:39 +0000

    db

--- jokko_finance/settings.py
+++ jokko_finance/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,6 +72,12 @@ WSGI_APPLICATION = 'jokko_finance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}
 
 
 STORAGES = {
